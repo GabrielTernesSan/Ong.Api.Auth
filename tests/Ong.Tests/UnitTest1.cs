0405a0494743eef645038c0b4fffec6becca44b9
William <william_w3_@hotmail.com>
2026-05-18 22:22:05 -0300

    correção testes

---
+++ tests/Ong.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Ong.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void StandardPipelineCheck_ShouldPass()
+        {
+            Assert.True(true);
+        }
+    }
+}
